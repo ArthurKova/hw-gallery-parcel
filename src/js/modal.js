2904c4d124e43a3febbb6adae3ccc29f18519c1d
ArthurKova <metaloutcome@gmail.com>
2023-05-24 16:10:07 +0300

    remove console.log from js file

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,7 +2,6 @@ import * as basicLightbox from 'basiclightbox';
 const pageGallery = document.querySelector('.gallery');
 
 export const pageModal = pageGallery.addEventListener('click', event => {
-  console.log(event);
   const instance = basicLightbox.create(`
   <div class="modal">
        <img src="${event.target.dataset.source}" alt="${event.target.alt}" width="100%" height="100%">
